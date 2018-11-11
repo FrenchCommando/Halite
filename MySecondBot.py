bf4be215be799632091e750659fb33e05bfeca32
FrenchCommando <martialrem@gmail.com>
2018-11-10 21:47:02 -0500

    Nedd to cut the tree

--- MySecondBot.py
+++ MySecondBot.py
@@ -12,7 +12,7 @@ from hlt.positionals import Direction, Position
 
 # This library allows you to generate random numbers.
 import random
-import time
+# import time
 from itertools import product
 
 # Logging allows you to save messages for yourself. This is required because the regular STDOUT
@@ -25,22 +25,20 @@ from itertools import chain
 # This game object contains the initial game state.
 game = hlt.Game()
 
-all_positions = [Position(y=y, x=x) for x in range(game.game_map.width) for y in range(game.game_map.height)]
 
-ship_status = {}
-ship_target = {}
+# shield_size = 5  # range of protection of the base
 
 
-class Status:
-    Exploring = 'exploring'
-    Collecting = 'collecting'
-    Returning = 'returning'  # with potentially different drop-offs
-    Building = 'building'  # build a new drop-off
-    Hunting = 'hunting'
-    Fighting = 'fighting'
+def normalize_position(position):
+    return position.x % game_map.width, position.y % game_map.height
 
 
-shield_size = 5  # range of protection of the base
+# should depend on threshold criterion
+me = game.me
+game_map = game.game_map
+
+height = game_map.height
+width = game_map.width
 
 
 # At this point "game" variable is populated with initial map data.
@@ -53,19 +51,8 @@ game.ready("MyPythonBot")
 logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
 
 
-def normalize_position(position):
-    return position.x % game_map.width, position.y % game_map.height
-
-
-# should depend on threshold criterion
-me = game.me
-game_map = game.game_map
-cleared_path = set()
-path_queue = set(normalize_position(entity.position) for entity in chain([me.shipyard], me.get_dropoffs()))
-adjacent_cells = set()
-
 while True:
-    start = time.time()
+    # start = time.time()
     # This loop handles each turn of the game. The game object changes every turn, and you refresh that state by
     #   running update_frame().
     game.update_frame()
@@ -112,54 +99,56 @@ while True:
         return False
 
 
-    # Scan for ennemies within shield_size of the shipyard
-    # Send ships to attack and destroy them (like a kamikaze)
-    # Status : Fighting
-    def shield_base():
-        for base_position in chain([me.shipyard], me.get_dropoffs()):
-            x_shipyard = base_position.position.x
-            y_shipyard = base_position.position.y
+    # # Scan for ennemies within shield_size of the shipyard
+    # # Send ships to attack and destroy them (like a kamikaze)
+    # # Status : Fighting
+    # def shield_base():
+    #     for base_position in chain([me.shipyard], me.get_dropoffs()):
+    #         x_shipyard = base_position.position.x
+    #         y_shipyard = base_position.position.y
+    #
+    #         def checked_positions():
+    #             """Generator for positions to check for ennemies"""
+    #             for x in range(-shield_size, shield_size):
+    #                 for y in range(-shield_size, shield_size):
+    #                     yield Position(
+    #                         x=(x_shipyard + x) % game_map.width,
+    #                         y=(y_shipyard + y) % game_map.height)
+    #
+    #         occupied_position = [position for position in checked_positions()
+    #                              if game_map[position].ship is not None
+    #                              and game_map[position].ship.owner != me.id]
+    #
+    #         # find the closest ally ship from a target
+    #         for target_position in occupied_position:
+    #             ships.sort(key=lambda my_ship: game_map.calculate_distance(my_ship.position, target_position))
+    #             for ship in ships.copy():
+    #                 if priority_move(ship, target_position):
+    #                     break
+    #             # what if not targeted ?
+    #     return
+    # shield_base()
 
-            def checked_positions():
-                """Generator for positions to check for ennemies"""
-                for x in range(-shield_size, shield_size):
-                    for y in range(-shield_size, shield_size):
-                        yield Position(
-                            x=(x_shipyard + x) % game_map.width,
-                            y=(y_shipyard + y) % game_map.height)
-
-            occupied_position = [position for position in checked_positions()
-                                 if game_map[position].ship is not None
-                                 and game_map[position].ship.owner != me.id]
-
-            # find the closest ally ship from a target
-            for target_position in occupied_position:
-                ships.sort(key=lambda my_ship: game_map.calculate_distance(my_ship.position, target_position))
-                for ship in ships.copy():
-                    if priority_move(ship, target_position):
-                        break
-                # what if not targeted ?
-        return
-    shield_base()
-
+    # that's useless
     basic_occupied_cells = set(next_occupied)
 
     # for x, y in basic_occupied_cells:
     #     logging.info('Occupied {} - {}'.format(x, y))
 
-    all_occupied = basic_occupied_cells.copy()
+    all_occupied = set()  # basic_occupied_cells.copy()
 
     if len(ships) != 0:
-
+        # not sure how to prioritize them
         ships.sort(key=lambda my_ship: my_ship.halite_amount, reverse=False)
 
         remaining_ships = [s for s in ships]
         # all we need is a mapping of ships to moves that make everyone happy
         # search in a tree that maximizes a utility function
-        # collision cuts the branch
-        # that sounds long and difficult
 
-        search_range = min(max(5, game.turn_number), game_map.height // 2 + 1)
+        search_range = min(max(5, game.turn_number // 2), game_map.height // 2 + 1)
+
+        # end = time.time()
+        # logging.info("Pre - FindMax-Threshold\t" + str(round((end - start) * 1000)))
 
         def checked_positions():
             """Generator for positions to check for ennemies"""
@@ -172,46 +161,53 @@ while True:
                             x=x_shipyard + x,
                             y=y_shipyard + y)
         max_halite = max(game_map[p].halite_amount for p in checked_positions())
-        low_threshold = min(100, max_halite / 10)
-        # logging.info('{} - {}'.format(max_halite, low_threshold))
+        low_threshold = round(min(50, max_halite / 10)) + 1
+        logging.info('{} - {}'.format(max_halite, low_threshold))
 
-        if game.turn_number != 1:
-            for c in adjacent_cells.copy():
-                x, y = c
-                p = Position(x=x, y=y)
-                if game_map[p].halite_amount <= low_threshold:
-                    adjacent_cells.remove(c)
-                    path_queue.add(c)
-        # all cells that are accessible from the shipyard or dropoffs
+        # if game.turn_number != 1:
+        #     for c in adjacent_cells.copy():
+        #         x, y = c
+        #         p = Position(x=x, y=y)
+        #         if game_map[p].halite_amount <= low_threshold:
+        #             adjacent_cells.remove(c)
+        #             path_queue.add(c)
+        # # all cells that are accessible from the shipyard or dropoffs
 
-        count = 100000
+        # end = time.time()
+        # logging.info("Pre - AdjacentCells\t" + str(round((end - start) * 1000)))
+        # cleared_path contains the cells with halite amount below the threshold
+        # contains tuples of (x,y)
+        adjacent_cells = set()
+        # it's a table not a set
+        cleared_path = [[False for y in range(height)] for x in range(width)]
+        # contains tuples of (x,y)
+        path_queue = set(normalize_position(entity.position) for entity in chain([me.shipyard], me.get_dropoffs()))
 
         while len(path_queue) != 0:
-            count -= 1
-            logging.info(count)
-            end = time.time()
-            logging.info(end - start)
-            if (end - start) > 1.9:
-                game.end_turn(command_queue)
-
             x, y = path_queue.pop()
+            cleared_path[x][y] = True
             p = Position(x=x, y=y)
-            cleared_path.add((x, y))
-            if game_map.calculate_distance(p, me.shipyard.position) < 10: # why not
-                for u in p.get_surrounding_cardinals():
-                    if game_map[u].halite_amount <= low_threshold:
-                        if (u.x, u.y) not in cleared_path:
-                            path_queue.add(normalize_position(u))
-                    else:
-                        adjacent_cells.add(normalize_position(u))
+            # if game_map.calculate_distance(p, me.shipyard.position) < 10:  # why not
+            for u in p.get_surrounding_cardinals():
+                u_x, u_y = normalize_position(u)
+                if game_map[u].halite_amount <= low_threshold:
+                    if not cleared_path[u_x][u_y]:
+                        path_queue.add((u_x, u_y))
+                else:
+                    adjacent_cells.add((u_x, u_y))
         # exploring ships will target adjacent cells and avoid cleared_path
         # returning ships must stay in cleared_path
         # collecting ships will stay in adjacent cells and avoid cleared_path
-        # for x,y in adjacent_cells:
+        # for x, y in adjacent_cells:
         #     logging.info('Adjacent {} - {}'.format(x, y))
         # for x,y in cleared_path:
         #     logging.info('Cleared {} - {}'.format(x, y))
 
+        # end = time.time()
+        # if (end - start) > 1.5:
+        #     game.end_turn(command_queue)
+        #     break
+        # logging.info("Post - AdjacentCells\t" + str(round((end - start) * 1000)))
 
         def find_closest_home(position):
             homes = list(chain([me.shipyard], me.get_dropoffs()))
@@ -225,13 +221,18 @@ while True:
             rep = []
             ship_position = ship.position
             ship_halite = ship.halite_amount
+
             to_assign = set(chain([Direction.Still], Direction.get_all_cardinals()))
+
+            for u in to_assign.copy():
+                if normalize_position(ship_position.directional_offset(u)) in basic_occupied_cells:
+                    to_assign.remove(u)
+
             map_to_cell = {d: ship_position.directional_offset(d) for d in to_assign}
 
             def assign(move):
-                destination = normalize_position(map_to_cell[move])
-                if move in to_assign and destination not in basic_occupied_cells:
-                    rep.append((move, destination))
+                if move in to_assign:
+                    rep.append((move, normalize_position(map_to_cell[move])))
                     to_assign.remove(move)
 
             def assign_still():
@@ -243,28 +244,23 @@ while True:
                 for move in moves:
                     assign(move)
 
-            if ship_halite == constants.MAX_HALITE:
-                go_home()
-                # for move, (x, y) in rep:
-                #     logging.info('Rep {} {} - {} - {}'.format(ship.id, move, x, y))
-                return rep
-
             cell_halite = game_map[ship_position].halite_amount
 
-            if ship_halite >= constants.MAX_HALITE * 3 / 4:
+            if ship_halite == constants.MAX_HALITE:
+                go_home()
+                assign_still()  # learn to wait in line
+                # return rep
+
+            elif ship_halite >= constants.MAX_HALITE * 9 / 10:
                 if cell_halite >= low_threshold:
                     assign_still()
                     go_home()
-                    # for move, (x, y) in rep:
-                    #     logging.info('Rep {} {} - {} - {}'.format(ship.id, move, x, y))
-                    return rep
+                    # return rep
                 else:
                     go_home()
                     if cell_halite != 0:
                         assign_still()
-                    # for move, (x, y) in rep:
-                    #     logging.info('Rep {} {} - {} - {}'.format(ship.id, move, x, y))
-                    return rep
+                    # return rep
 
             if cell_halite >= low_threshold:
                 assign_still()
@@ -273,14 +269,10 @@ while True:
                 x, y = target_position
                 my_position = Position(x=x, y=y)
                 distance = game_map.calculate_distance(ship_position, my_position)
-                # if distance == 0:
-                #     return 100000000000
-                return 1 * game_map[my_position].halite_amount - 100 * distance
+                return 1 * game_map[my_position].halite_amount - 10 * distance
 
-            if len(adjacent_cells) >= 0:
+            if len(adjacent_cells) > 0:
                 l_adjacent_cells.sort(key=my_utility, reverse=True)
-                # i_l_adj = iter(l_adjacent_cells)
-                # tx, ty = next(i_l_adj)
                 tx, ty = max(l_adjacent_cells, key=my_utility)
                 moves = game_map.get_unsafe_moves(ship_position, Position(x=tx, y=ty))
                 for move in moves:
@@ -290,13 +282,29 @@ while True:
                 if game_map[map_to_cell[move]].halite_amount >= low_threshold:
                     assign(move)
 
-            for move in to_assign.copy():
-                assign(move)
+            if len(rep) == 0:
+                l_to_assign = list(to_assign)
+
+                def halite_in_target(move):
+                    u = map_to_cell[move]
+                    return game_map[u].halite_amount
+                l_to_assign.sort(key=halite_in_target, reverse=True)
+
+                for move in l_to_assign:
+                    assign(move)
+
+            assign_still()  # learn to wait in line
 
             # for move, (x, y) in rep:
             #     logging.info('Rep {} {} - {} - {}'.format(ship.id, move, x, y))
 
             return rep
+
+        # end = time.time()
+        # logging.info("Pre - Mapping \t" + str(round((end - start) * 1000)))
+        # end = time.time()
+        # if (end - start) > 1.5:
+        #     break
 
         ship_mapping = {ship.id: decision(ship) for ship in remaining_ships}
 
@@ -311,13 +319,16 @@ while True:
         #     game.end_turn(command_queue)
         #     break
 
-        # solved = False
-        count = 100000
+        # end = time.time()
+        # logging.info("Pre - tree\t" + str(round((end - start) * 1000)))
+        solved = False
+        # this is very bad - youre going through the whole tree - not even cutting the branches
         for u in product(*[ship_mapping[ship.id] for ship in remaining_ships]):
-            count -= 1
-            if count == 0:
-                break
-            all_occupied = basic_occupied_cells.copy()
+            # end = time.time()
+            # if (end - start) > 1.5:
+            #     game.end_turn(command_queue)
+            #     break
+            all_occupied = set()  # basic_occupied_cells.copy()
 
             wrong = False
             for (move, (cell_x, cell_y)) in u:
@@ -329,40 +340,54 @@ while True:
             if wrong:
                 continue
             choose_moves(u)
-            # solved = True
+            solved = True
 
-            # end = time.time()
-            # logging.info(end-start)
-            # if (end - start) > 1.9:
-            #     game.end_turn(command_queue)
-            #     break
             break
 
-        logging.info(count)
-        end = time.time()
-        logging.info(end-start)
-        if (end - start) > 1.9:
-            game.end_turn(command_queue)
+        # logging.info(count)
+        # end = time.time()
+        # logging.info(end-start)
+        # if (end - start) > 1.9:
+        #     game.end_turn(command_queue)
 
-        # if not solved:
+        # end = time.time()
+        # logging.info("Post Tree\t" + str(round((end - start) * 1000)))
+        # end = time.time()
+        # if (end - start) > 1.5:
+        #     game.end_turn(command_queue)
+        #     break
+
+        if not solved:
+            for ship in remaining_ships:
+                logging.info("Ship {} - position {} {}".format(ship.id, ship.position.x, ship.position.y))
+            logging.info("Not Solvec")
+            for x, y in adjacent_cells:
+                logging.info('Adjacent {} - {}'.format(x, y))
+            for ship in remaining_ships:
+                command_queue.append(ship.move(Direction.Still))
+                all_occupied.add(normalize_position(ship.position))
             # do something
             # pass
 
     # If the game is in the first 200 turns and you have enough halite, spawn a ship.
     # Don't spawn a ship if you currently have a ship at port, though - the ships will collide.
-    if (constants.MAX_TURNS - game.turn_number >= 200 or game.turn_number <= 200) \
+    if (constants.MAX_TURNS - game.turn_number >= 200 or game.turn_number <= 250) \
             and me.halite_amount >= constants.SHIP_COST \
             and not normalize_position(me.shipyard.position) in all_occupied \
+            and not normalize_position(me.shipyard.position) in basic_occupied_cells \
             and len(me.get_ships()) <= game_map.height:
         command_queue.append(me.shipyard.spawn())
 
     # Send your moves back to the game environment, ending this turn.
     game.end_turn(command_queue)
 
+    # end = time.time()
+    # logging.info("\t" + str(round((end - start) * 1000)))
+
     # for u in command_queue:
     #     logging.info(u)
 
-    if not game.turn_number < 50: #(constants.MAX_TURNS - game.game_map.height / 2):
+    if not game.turn_number < 100:  # (constants.MAX_TURNS - game.game_map.height / 2):
         break
 
     # Scan for ship that are already heavily loaded
@@ -397,6 +422,22 @@ hunter = True
 #     hunter = True
 # # end : stupid hunting strategy
 #
+
+all_positions = [Position(y=y, x=x) for x in range(game.game_map.width) for y in range(game.game_map.height)]
+
+
+ship_status = {}
+ship_target = {}
+
+
+class Status:
+    Exploring = 'exploring'
+    Collecting = 'collecting'
+    Returning = 'returning'  # with potentially different drop-offs
+    Building = 'building'  # build a new drop-off
+    Hunting = 'hunting'
+    Fighting = 'fighting'
+
 
 def less_naive_navigate(ship, destination):
     """
@@ -454,7 +495,8 @@ def even_less_naive_navigate(ship, destination):
 """ <<<Game Loop>>> """
 
 
-while game.turn_number < (constants.MAX_TURNS - game.game_map.height / 2):
+# while game.turn_number < (constants.MAX_TURNS - game.game_map.height / 2):
+while game.turn_number < (constants.MAX_TURNS - max(len(me.get_ships()), game_map.height) // 2):
     # This loop handles each turn of the game. The game object changes every turn, and you refresh that state by
     #   running update_frame().
     game.update_frame()
